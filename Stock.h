5df9348d7cb71863227f2531adaf66969a8de18f
pnirbana <putrasabilillah04@gmail.com>
2025-12-25 01:30:30 +0700

    Update Stock.h

--- Stock.h
+++ Stock.h
@@ -3,12 +3,15 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 struct Product {
     int id;
     string name;
     int stock;
+    int price;
+    string category;
 };
 
 struct Node {
@@ -23,16 +26,15 @@ void createTree(adrNode &root);
 adrNode newNode(Product x);
 void insertNode(adrNode &root, Product x);
 adrNode searchNode(adrNode root, int id);
-adrNode findMin(adrNode root);
 adrNode deleteNode(adrNode root, int id);
 void inOrder(adrNode root);
 int countProduct(adrNode root);
 int totalStock(adrNode root);
-void showMin(adrNode root);
-void showMax(adrNode root);
 void stockLow(adrNode root);
 void searchName(adrNode root, string name);
 void updateProduct(adrNode root, int id);
+void showAllCategories(adrNode root);
+void searchCategory(adrNode root, string category, bool &found);
+void filterByPrice(adrNode root, int minPrice, int maxPrice, bool &found);
 
 #endif
-
